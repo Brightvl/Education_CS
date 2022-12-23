02b9b460848dc6e5042d7f5096815b7b1d74e8bb
Brightvl <ziro-x-100@mail.ru>
2022-12-23 22:43:42 +0300

    решал задачки

--- All_TestWork/T_001_if/Program.cs
+++ All_TestWork/T_001_if/Program.cs
@@ -1,3 +1,13 @@
-﻿int num = int.Parse(Console.ReadLine());
-string txt = ( num > 0)? "больше" : "меньше";
-Console.WriteLine(txt);
+﻿/* Генерируется случайное целое число n в интервале [ 20 – 99 ].
+Проверяется, делится ли оно на введенное с клавиатуры целое число d
+(используйте тернарный оператор). */
+
+Random rnd = new Random();
+int rand = rnd.Next(19, 100);
+int num = int.Parse(Console.ReadLine());
+
+string del1 = (rand / num > 0) ? "Делится" : "нет";
+
+Console.WriteLine(rand);
+Console.WriteLine(del1);
+Console.WriteLine(num);
---
+++ All_TestWork/T_002/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Генерируются два случайных целых числа a и b в интервале [10 – 30].
+Если a > b, выводится их разность, eсли a < b, выводится сумма,
+eсли a == b, выводится сообщение “числа равны”. (оператор if) */
+
+Console.Clear();
+
+Random rnd = new Random();
+
+int a = rnd.Next(10, 30);
+
+int b = rnd.Next(10, 30);
+
+if (a > b)
+{
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    Console.WriteLine(a - b);
+}
+else if (a < b)
+{
+    Console.WriteLine(b);
+    Console.WriteLine(a);
+    Console.WriteLine(b + a);
+}
+else
+{
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    Console.WriteLine("Числа равны");
+}
