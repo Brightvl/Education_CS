e05ef691237bc5a283c975e67d88233efa803a42
Iaroslav S <ziro-x-100@mail.ru>
2023-01-20 01:07:40 +0300

    6 HomeWork

--- All_HomeWork/HomeWork_006/HW6.cs
+++ All_HomeWork/HomeWork_006/HW6.cs
@@ -1,9 +1,8 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
-Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// /*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3 */
-
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3 */
 
 void Number()
 {
@@ -26,42 +25,127 @@ void Number()
     }
 }
 
+Number(); //Задача №41
+
 /* Задача 43: Напишите программу, которая найдёт точку пересечения 
 двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 значения b1, k1, b2 и k2 задаются пользователем.
-x
-b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5) */
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0,5; -0,5) */
 
 
-Console.Write("Уравнение отрезков: y = k * x + b ");
-Console.Write("Введите k1");
-int k1 = int.Parse(Console.ReadLine());
-Console.Write("Введите b1");
-int b1 = int.Parse(Console.ReadLine());
-Console.Write("Введите k2");
-int k2 = int.Parse(Console.ReadLine());
-Console.Write("Введите b2");
-int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 1. Введите k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 1. Введите b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 2. Введите k2");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Прямая 2. Введите b2");
+double b2 = double.Parse(Console.ReadLine());
 
-if (k1 != k2)
+void CoordinatePointLine(double k1, double k2, double b1, double b2)
 {
-    int x =
+    Console.Write("Уравнение пересечения прямых:\ny = k1 * x + b1\ny = k2 * x + b1\n\n");
+    if (k1 != k2)
+    {
+        double x = (b1 - b2) / (k2 - k1);
+        double y = k1 * x + b1;
+        // double y2 = k2 * x + b2;                 //y2 == y1
+        Console.Write($"Координаты точки пересечения прямых:\nx = {x} y1 = {y}");
+    }
+    else if (k1 == k2 && b1 != b2)
+    {
+        Console.Write("Две прямые параллельны, a значит, они никогда не пересекутся и система не будет иметь решений");
+
+    }
+    else if (k1 == k2 && b1 == b2)
+    {
+        Console.Write("Две прямые совпали, каждая точка будет решением, а у системы будет бесчисленное множество решений.");
+    }
 }
-else if (k1 == k2 && b1 != b2)
-{
-    /*     Две прямые могут быть параллельны, а значит, они никогда не 
-        пересекутся и система не будет иметь решений. 
-        y = k1 * x + b1, y = k2 * x + b2; 
-        y - y = k1*x + b1 - (k2*x + b2)
-        0 = 
-        x
-        */
-}
-else if (k1 == k2 && b1 == b2)
-{
-    /* Две прямые могут совпасть, и тогда каждая точка будет решением, 
-    а у системы будет бесчисленное множество решений. */
-}
-//Number(); //Задача №41
+
+CoordinatePointLine(k1,k2,b1,b2);
 
 
+
+
+// РАЗРАБОТКИ 
+
+
+// string [] sub = {"k1","k2","b1","b2"};
+// int i = 0;
+// while (i<4)
+// {
+//     Console.WriteLine($"Введите {sub[i]}");
+//     string something = Console.ReadLine();
+//     Console.Clear();
+//     Console.Write("Уравнение пересечения прямых:\ny = k1 * x + b1\ny = k2 * x + b1\n\n");
+    
+    
+//     i++;
+// }
+
+
+
+
+// int[] GetFunctionParametersFromUser()
+// {
+//     int[] result = new int[2];
+//     Console.Write("Введите через запятую значения переменных для уравнения A*x + B. Например '3,1' -> ");
+//     string userInput = Console.ReadLine();
+//     int i = 0;
+//     foreach (string numberStr in userInput.Split(','))
+//     {
+//         if (! int.TryParse(numberStr, out result[i]))
+//         {
+//             Console.WriteLine($"Не удалось распарсить '{numberStr}', число задано как 0");
+//         }
+//         i++;
+//     }
+//     return result;
+// }
+
+
+// double[] FindIntersectionPoints(int[] func1, int[] func2)
+// {
+//     double[] combinedFunc = new double[2];
+//     combinedFunc[0] = func1[0] - func2[0];
+//     combinedFunc[1] = func1[1] - func2[1];
+//     double x = -(combinedFunc[1]/combinedFunc[0]);
+//     double y = func1[0] * x + func1[1];
+//     return new double[] {x, y};
+// }
+
+// Console.WriteLine("Уравнения y = k1*x + b1 и y = k2*x + b2");
+// Console.WriteLine("Для первого уравнения");
+// int[] y1 = GetFunctionParametersFromUser();
+// Console.WriteLine("Для второго уравнения");
+// int[] y2 = GetFunctionParametersFromUser();
+
+// if (y1[0] == y2[0])
+// {
+//     Console.WriteLine("Угловые коэффициенты равны, функции не пересекаются т.к. Параллельны");
+// }
+// else
+// {
+//     Console.WriteLine($"Функции пересекаются в точке ({String.Join(", ", FindIntersectionPoints(y1, y2))})");
+// }
+
+// double[] point =  FindIntersectionPoints(y1, y2);
+// char[,] graph = new char[10, 10];
+
+// int xStart = (int)point[0] -5;             //x координата точки пересечения 
+// int yStart = (int)point[1] -5;             //y координата
+
+// for (int i=0;i<graph.GetLength(0);i++)
+// {
+//     for (int j=0;j<graph.GetLength(1);j++)
+//     {
+//         if (j+yStart == Convert.ToInt32(y1[0]*(xStart+ i) + y1[1]) || j+yStart == Convert.ToInt32(y2[0]*(xStart+i) + y2[1]))// graph[i,j] = '*';
+//         {
+//             Console.Write('*');
+//         }
+//         else Console.Write(' ');
+//     }
+//     Console.WriteLine();
+// }
---
+++ All_Seminars/Seminar_006/Program.cs
@@ -0,0 +1,204 @@
+﻿
+//Задача 40. Напишите программу, которая принимает на вход три числа и проверяет,
+//может ли существовать треугольник со сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+// Console.WriteLine("Введите первое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+// if (number < number2 + number3)
+// {
+//     if (number2 < number + number3)
+//     {
+//         if (number3 < number2 + number)
+//         {
+//             Console.WriteLine("Да, такой треугольник может существовать");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Нет, такой треугольник не может существовать");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Нет, такой треугольник не может существовать");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Нет, такой треугольник не может существовать");
+// }
+
+/* Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10 */
+
+
+// Console.Clear();
+
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array)
+// {
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] Array = CreateRandomArray(Convert.ToInt32(Math.Log2(N)) + 1, 0, 1);
+// int i = 0;
+// while (N > 0)
+// {
+//     Array[Array.Length - i - 1] = N % 2;
+//     N = N / 2;
+//     i++;
+// }
+// ShowArray(Array);
+
+
+// void DecimalBinary(int numb)
+// {
+//     string residue = string.Empty;
+//     while (numb > 0)
+//     {
+//         residue = Convert.ToString(numb % 2) + residue;
+//         numb /= 2;
+//         Console.WriteLine(numb);
+//     }
+//     Console.WriteLine(residue);
+// }
+
+// DecimalBinary(14);
+
+
+/*
+ * Console.WriteLine("Введите число");
+int number_10 = int.Parse(Console.ReadLine());  //10
+int i = number_10;                              
+string number_02 = "";                          
+while (i > 0)
+{
+    number_02 = i % 2 + number_02;  //остаток от деления        
+    i = i / 2;                      //деление самого числа       
+}
+Console.WriteLine(number_02);
+//10 = 2+2+2+2+2(0)    (0)  ^
+//5 = 2+2+(1)          (1)  |    
+//2 = 2(0)             (0)  |
+//1 = 0(1)             (1)  |
+*/
+
+
+
+
+
+
+
+/* Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = 0;
+// int number2 = 1;
+// int number3 = 0;
+// int j = 0;
+// Console.WriteLine(number1);
+// Console.WriteLine(number2);
+// while (j < number)
+// {
+//     number3 = number1 + number2;
+//     Console.WriteLine(number3);
+//     number1 = number2;
+//     number2 = number3;
+//     j++;
+// }
+
+// int[] Fib = new int[size];
+// Fib[0] = 0;
+// Fib[1] = 1;
+// for (int i = 2; i < size; i++)
+// {
+//     Fib[i] = Fib[i - 2] + Fib[i - 1];
+// }
+
+
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 2)
+// {
+//     Console.WriteLine("Введите число больше 2");
+// }
+// else
+// {
+//     int number1 = 0;
+//     int number2 = 1;
+//     int number3 = 0;
+//     Console.WriteLine(number1);
+//     Console.WriteLine(number2);
+//     for (int j = 0; j < number; j++)
+//     {
+//         number3 = number1 + number2;
+//         Console.WriteLine(number3);
+//         number1 = number2;
+//         number2 = number3;
+//     }
+// }
+
+
+
+
+
+
+/* Задача 45: Напишите программу, которая будет создавать
+ копию заданного массива с помощью поэлементного копирования.
+ */
+
+//  int[] array = GetArray(10, 0, 10);
+// WriteLine(String.Join(" ", array));
+
+// int[] copyArray=CopyArray(array);
+// WriteLine(String.Join(" ", copyArray));
+
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
+
+// int[] CopyArray(int[] inArray)
+// {
+//     int[] result = new int[inArray.Length];
+//     for (int i = 0; i < inArray.Length; i++)
+//     {
+//         result[i] = inArray[i];
+
+//     }
+//     return result;
+// }
+
+
