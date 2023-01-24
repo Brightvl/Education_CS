e8fdab748f95236047714a22a5691e4cf1d32e43
Iaroslav S <ziro-x-100@mail.ru>
2023-01-24 23:16:11 +0300

    7 семминар

--- All_HomeWork/HomeWork_006/HW6.cs
+++ All_HomeWork/HomeWork_006/HW6.cs
@@ -1,8 +1,8 @@
-﻿// /*Задача 41: Пользователь вводит с клавиатуры M чисел. 
-// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3 */
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
 
 void Number()
 {
@@ -26,6 +26,43 @@ void Number()
 }
 
 Number(); //Задача №41
+//Альтернатиыное правильно решение 
+
+/* Функция разделяет строку на подстроки и добавляет в массив. 
+При этом возвращает целочисленный массив
+Разделение с помощью.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries)
+В аргументах указана запятая и пробел, а 3 опция удаляет пустые значения.
+В итоге в массив записываются только значние без запятых, пробелов и пустых занчений.*/
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+}
+
+/* Находит положительные значения в int массиве 
+foreach - цикл который перебирает каждое значение из массива
+переменная item если больше 0 то увеличиваем значение внутри массива*/
+int GerCountPsitiveElements(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item > 0) count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Ввыедите числа через пробел");
+int[] numbers = GetArrayFromString(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше 0 -> {GerCountPsitiveElements(numbers)}");
+
+
 
 /* Задача 43: Напишите программу, которая найдёт точку пересечения 
 двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
@@ -34,39 +71,58 @@ Number(); //Задача №41
 b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0,5; -0,5) */
 
 
-Console.WriteLine("Прямая 1. Введите k1");
-double k1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Прямая 1. Введите b1");
-double b1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Прямая 2. Введите k2");
-double k2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Прямая 2. Введите b2");
-double b2 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Прямая 1. Введите k1");
+// double k1 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Прямая 1. Введите b1");
+// double b1 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Прямая 2. Введите k2");
+// double k2 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Прямая 2. Введите b2");
+// double b2 = double.Parse(Console.ReadLine());
 
-void CoordinatePointLine(double k1, double k2, double b1, double b2)
+// void CoordinatePointLine(double k1, double k2, double b1, double b2)
+// {
+//     Console.Write("Уравнение пересечения прямых:\ny = k1 * x + b1\ny = k2 * x + b1\n\n");
+//     if (k1 != k2)
+//     {
+//         double x = (b1 - b2) / (k2 - k1);
+//         double y = k1 * x + b1;
+//         // double y2 = k2 * x + b2;                 //y2 == y1
+//         Console.Write($"Координаты точки пересечения прямых:\nx = {x} y1 = {y}");
+//     }
+//     else if (k1 == k2 && b1 != b2)
+//     {
+//         Console.Write("Две прямые параллельны, a значит, они никогда не пересекутся и система не будет иметь решений");
+
+//     }
+//     else if (k1 == k2 && b1 == b2)
+//     {
+//         Console.Write("Две прямые совпали, каждая точка будет решением, а у системы будет бесчисленное множество решений.");
+//     }
+// }
+
+// CoordinatePointLine(k1, k2, b1, b2);
+
+//альт
+
+//определяет точку пересечния прямых на плосоксти с заданными данными
+double[] GetPoint(double inB1, double inK1, double inB2, double inK2)
 {
-    Console.Write("Уравнение пересечения прямых:\ny = k1 * x + b1\ny = k2 * x + b1\n\n");
-    if (k1 != k2)
-    {
-        double x = (b1 - b2) / (k2 - k1);
-        double y = k1 * x + b1;
-        // double y2 = k2 * x + b2;                 //y2 == y1
-        Console.Write($"Координаты точки пересечения прямых:\nx = {x} y1 = {y}");
-    }
-    else if (k1 == k2 && b1 != b2)
-    {
-        Console.Write("Две прямые параллельны, a значит, они никогда не пересекутся и система не будет иметь решений");
-
-    }
-    else if (k1 == k2 && b1 == b2)
-    {
-        Console.Write("Две прямые совпали, каждая точка будет решением, а у системы будет бесчисленное множество решений.");
-    }
+    double[] result = new double[2];
+    result[0] = (inB2 - inB1) / (inK1 - inK2);
+    result[1] = inK1 * result[0] + inB1;
+    return result;
 }
 
-CoordinatePointLine(k1,k2,b1,b2);
-
-
+Console.Clear();
+Console.Write("Введите через пробел b1, k1, b2, k2: ");
+string[] f = Console.ReadLine().Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
+double b1 = double.Parse(f[0]);
+double k1 = double.Parse(f[1]);
+double b2 = double.Parse(f[2]);
+double k2 = double.Parse(f[3]);
+//Join выводит массив значения массива через пробел в строку
+Console.WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2)));
 
 
 // РАЗРАБОТКИ 
@@ -80,8 +136,8 @@ CoordinatePointLine(k1,k2,b1,b2);
 //     string something = Console.ReadLine();
 //     Console.Clear();
 //     Console.Write("Уравнение пересечения прямых:\ny = k1 * x + b1\ny = k2 * x + b1\n\n");
-    
-    
+
+
 //     i++;
 // }
 
---
+++ All_Seminars/Seminar_007/S7.cs
@@ -0,0 +1,113 @@
+﻿/* Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+
+//Содание массива M+N
+int[,] GetArrayPlusMN(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+//Вывести массив
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Console.Clear();
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = GetArrayPlusMN(rows, columns, 0, 4);
+// PrintArray(array);
+
+
+/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4 */
+
+//Создает массив с рандомными числами
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+//находит одинаковые четные индексы и возводит их в квадрат
+int[,] SquarePositivePlaces(int[,] inArray)
+{
+    int[,] array = inArray;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+// Console.WriteLine("Измененный массив, где элементы с положительными индексами - квадрат: ");
+//int[,] array = GetArray(10, 10, 0, 4);
+// PrintArray(array);
+// SquarePositivePlaces(array);
+// Console.WriteLine();
+// PrintArray(array);
+
+
+/* Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12 */
+
+int SummGeneralDiagonal(int[,] inArray)
+{
+    int result = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result += inArray[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+int[,] array = GetArray(10, 10, 0, 4);
+PrintArray(array);
+Console.Write(SummGeneralDiagonal(array));
+
