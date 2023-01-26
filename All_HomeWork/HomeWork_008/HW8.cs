d94abdb77c040fdab95e3eb8ae12c770c1d2cc25
Iaroslav S <ziro-x-100@mail.ru>
2023-01-27 00:00:05 +0300

    ДЗ половина

---
+++ All_HomeWork/HomeWork_008/HW8.cs
@@ -0,0 +1,140 @@
+﻿/* 54 Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой 
+строки двумерного массива. */
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+int[,] ArrayStringReduction(int[,] array)
+{
+
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int coll = 0; coll < array.GetLength(1); coll++)
+        {
+            for (int k = coll + 1; k < array.GetLength(1); k++)
+            {
+                if (array[row, k] > array[row, coll])
+                {
+                    int n = array[row, coll];
+                    array[row, coll] = array[row, k];
+                    array[row, k] = n;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+/* 56 Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+ */
+
+void MinSummStringInArray(int[,] array)
+{
+    int min = 0;
+    int sum = 0;
+    int num = 0;
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int coll = 0; coll < array.GetLength(1); coll++)
+        {
+            sum += array[row, coll];
+        }
+        Console.WriteLine(sum);
+        if (min < sum)
+        {
+            min = sum;
+            num = row+1;
+        }
+        sum = 0;
+    }
+    Console.WriteLine(num);
+}//Сделать перебор и в переменную записать наименьшее
+
+
+
+/* 60 Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно 
+выводить массив, добавляя индексы каждого элемента. */
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] GetThirdArray(int x, int y, int z, int minValue, int maxValue)
+{
+    int[,,] result = new int[x, y, z];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(2); k++)
+            {
+                result[i, j, k] = new Random().Next(minValue, maxValue);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray1(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($"{inArray[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] array = GetArray(5, 4, 1, 9);
+PrintArray(array);
+Console.WriteLine();
+ArrayStringReduction(array);                        //54
+PrintArray(array);
+Console.WriteLine();
+
+MinSummStringInArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+
+
+
+
+
+int[,,] arrayThird = GetThirdArray(4, 4, 4, 10, 100); //60
+PrintArray1(arrayThird);
+
---
+++ All_Seminars/Seminar_008/S8.cs
@@ -0,0 +1,94 @@
+﻿/* 53 Задайте двумерный массив. Напишите программу,которая 
+поменяет местами первую и последнюю строку массива
+ */
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+int[,] SwapArray(int[,] array)
+{
+    int n;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        n = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = n;
+    }
+    return array;
+}
+
+
+
+/* 55 Задайте двумерный массив. Напишите программу,которая 
+заменяет строки на столбцы. В случае, если 
+это невозможно, программа должна вывести сообщение 
+для пользователя
+ */
+
+int[,] ReverseArray(int[,] array)
+{
+    int[,] result = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[j, i] = array[i, j];
+        }
+    }
+   
+    return result;
+}
+
+
+/*  57 Составить частотный словарь элементовдвумерного массива. 
+Частотный словарь содержитинформацию о том, сколько раз 
+встречается элементвходных данных
+ */
+
+/* Задача 59: Задайте двумерный массив из целых чисел. Напишите 
+программу, которая удалит строку и столбец, напересечении которых 
+расположен наименьший элементмассива.Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получимследующий массив:
+9 4 2
+2 2 6
+3 4 7 */
+
+/* 61 Вывести первые N строк треугольникаПаскаля. 
+Сделать вывод в виде равнобедренноготреугольника */
+
+
+int[,] array = GetArray(5, 4, 1, 9);
+PrintArray(array);
+Console.WriteLine();
+SwapArray(array);                       //#53
+PrintArray(array);
+int[,] newArray = ReverseArray(array); //#55 Обязательно нужно перезаписать
+// потому что мы возвращаем значение а не массив
+Console.WriteLine();
+PrintArray(newArray);
